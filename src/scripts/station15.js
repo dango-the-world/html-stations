9199543196e09e5dbf64db8bbec1da13899ec91c
dango-the-world <z.rohaso@gmail.com>
2024-05-30 23:11:02 +0900

    update station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -2,15 +2,53 @@ async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
-  const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
-  ];
-  const result = await test()
-  return await result
+  // const userList = [
+  //   {
+  //     id: 1,
+  //     first_name: "優",
+  //     family_name: "大木",
+  //     affiliation: "TechTrain",
+  //     is_student: false,
+  //   },
+  //   {
+  //     id: 2,
+  //     first_name: "太郎",
+  //     family_name: "山田",
+  //     affiliation: "HogeHoge大学",
+  //     is_student: true,
+  //   },
+  // ];
+  const result = await test();
+  console.log(result + "これはテスト");
+
+  return await result;
 }
 
 function test() {
-  return
-}
+  const userList = [
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
+  ];
 
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const testList = userList.map((data) => {
+        return { full_name: data.family_name + " " + data.first_name };
+      });
+      console.log(testList);
+      resolve(testList);
+    }, 3000);
+  });
+}
